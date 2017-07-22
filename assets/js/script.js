da34778e7dd140e9e43b5b11349bb0823ea0e7af
Rachel <racheldchristian@gmail.com>
2017-07-22 15:14:20 -0500

    changed when navbar shrinks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(window).scroll(function() {
-    if ($(document).scrollTop() > 100) {
+    if ($(document).scrollTop() > 30) {
         $('nav').addClass('shrink');
     } else {
         $('nav').removeClass('shrink');
