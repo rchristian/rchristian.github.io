afcddb6a2c9f484b0a2fc53fc24fe8374b1a3101
Rachel <racheldchristian@gmail.com>
2017-07-21 21:11:40 -0500

    more nav styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $(window).scroll(function() {
-  if ($(document).scrollTop() > 100) {
-    $('nav').addClass('shrink');
-  } else {
-    $('nav').removeClass('shrink');
-  }
+    if ($(document).scrollTop() > 100) {
+        $('nav').addClass('shrink');
+    } else {
+        $('nav').removeClass('shrink');
+    }
 });
